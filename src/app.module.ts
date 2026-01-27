a1d43c37327bd977665ed66cf52a3e5319372c03
Julio Fernandes <juliocesarmf.1412@gmail.com>
2026-01-27 17:53:43 -0300

    fix: definindo app base (2)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabasesModule } from './databases/databases.module';
 import { ConfigModule } from '@nestjs/config';
-import { CompaniesModule } from './companies/companies.module';
 
 @Module({
-  imports: [
-    DatabasesModule,
-    ConfigModule.forRoot({ isGlobal: true }),
-    CompaniesModule,
-  ],
+  imports: [DatabasesModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
